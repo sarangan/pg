6549d37dbb3bec16c21917fe863714a98e504592
sara <ksara@PIMAGLKSARA8881.users.sphinet.com.sg>
2017-05-15 18:11:35 +0800

    pdf download need to fix

--- src/actions/PropertyListActions.js
+++ src/actions/PropertyListActions.js
@@ -32,6 +32,7 @@ export function fetchPropList() {
 
 export function generateReport(property_id) {
 
+
   var url = config.REPORT_ENDPOINT_URL + 'generatereport';
   axios({
           method: 'post',
@@ -41,10 +42,37 @@ export function generateReport(property_id) {
            },
           data: {
             property_id
-          }
+          },
+          responseType:'stream'
         })
         .then(function (response) {
           console.log(response);
+
+          let data = response;
+          let filename = "report.pdf";
+          let mime = 'application/octet-stream';
+
+
+            var blob = new Blob([data], {type: mime || 'application/octet-stream'});
+            if (typeof window.navigator.msSaveBlob !== 'undefined') {
+               // IE workaround for "HTML7007: One or more blob URLs were
+               // revoked by closing the blob for which they were created.
+               // These URLs will no longer resolve as the data backing
+               // the URL has been freed."
+               window.navigator.msSaveBlob(blob, filename);
+            }
+            else {
+               var blobURL = window.URL.createObjectURL(blob);
+               var tempLink = document.createElement('a');
+               tempLink.href = blobURL;
+               tempLink.setAttribute('download', filename);
+               tempLink.setAttribute('target', '_blank');
+               document.body.appendChild(tempLink);
+               tempLink.click();
+               document.body.removeChild(tempLink);
+             }
+
+
 
         })
         .catch(function (error) {
