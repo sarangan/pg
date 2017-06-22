c6acdb87677c794731493d86e3f37ed7462dc314
sara <sarangan12@gmail.com>
2017-06-22 21:28:27 +0800

    update changes on config

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var config = {
-  SERVER_IMAGE_PATH : 'http://52.39.72.94:3000/images/',
-  SERVER_RPT_IMAGE_PATH : 'http://52.39.72.94:3000/images/reportlogos/',
-  ENDPOINT_URL: 'http://52.39.72.94:3000/Property/',
-  REPORT_ENDPOINT_URL: 'http://52.39.72.94:3000/Report/',
-  SERVER_ENDPOINT_URL: 'http://52.39.72.94:3000/',
+  SERVER_IMAGE_PATH : 'http://propertyground.co.uk:1337/images/',
+  SERVER_RPT_IMAGE_PATH : 'http://propertyground.co.uk:1337/images/reportlogos/',
+  ENDPOINT_URL: 'http://propertyground.co.uk:1337/Property/',
+  REPORT_ENDPOINT_URL: 'http://propertyground.co.uk:1337/Report/',
+  SERVER_ENDPOINT_URL: 'http://propertyground.co.uk:1337/',
 }
 
 module.exports = config;
